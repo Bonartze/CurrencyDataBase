3205a944a70b704588698a10df9884f444b68827
Ivan Kirilin <iokirilin@mail.ru>
2023-11-19 19:18:18 +0300

    Added full build assembly

---
+++ program_build/CMakeFiles/untitled87_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled87_autogen"
+  "untitled87_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/untitled87_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
