3312c127f0158d042bd598113e9bf18c30b93ecb
Ivan Kirilin <iokirilin@mail.ru>
2023-11-19 19:41:41 +0300

    Check

--- ConnectionTool.cpp
+++ ConnectionTool.cpp
@@ -1,7 +1,7 @@
 #include "ConnectionTool.hpp"
 
 pqxx::connection ConnectionTool::connect(
-        "user=postgres password=123 host=89.204.138.144 port=3389 dbname=postgres target_session_attrs=read-write");
+        "user=postgres password=123 host=127.0.0.1 port=5432 dbname=postgres target_session_attrs=read-write");
 
 pqxx::connection &ConnectionTool::GetConnect() {
     return connect;
